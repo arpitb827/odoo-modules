29af21331a35a1ed4a911497bc600db6c2577725
arpit <arpit.bansal@bistasolutions.com>
2017-12-18 00:38:30 +0530

    correct indentations and removed prints

--- mail_attachment_password/models/template_attachment_password.py
+++ mail_attachment_password/models/template_attachment_password.py
@@ -18,28 +18,26 @@ class MailTemplate(models.Model):
 		if self.is_report_protected and self.report_password:
 			for key,value in res.values()[0].iteritems():
 				if key=='attachments':
-					# print value
 					password = str(self.report_password)
 					name = value[0][0]
-					print type(value)
 					bin_data = value[0][1]
 					new_pdf = base64.b64decode(bin_data)
 					new_file = open('/tmp/test.pdf','wb')
-	                new_file = new_file.write(new_pdf)
-	                pdffile = open('/tmp/test.pdf','rb')
-	                pdfreader = PyPDF2.PdfFileReader(pdffile)
-	                pdfwriter = PyPDF2.PdfFileWriter()
+					new_file = new_file.write(new_pdf)
+					pdffile = open('/tmp/test.pdf','rb')
+					pdfreader = PyPDF2.PdfFileReader(pdffile)
+					pdfwriter = PyPDF2.PdfFileWriter()
 
-	                for pagenum in range(pdfreader.numPages):
-	                    pdfwriter.addPage(pdfreader.getPage(pagenum))
-	                 
-	                pdfwriter.encrypt(password)
-	                resulltpdf = open('/tmp/'+str(value[0][0]),'wb')
-	                pdfwriter.write(resulltpdf)
-	                resulltpdf.close()
-	                with open('/tmp/'+str(value[0][0]), "rb") as pdf_file:
-	                    encoded_string = base64.b64encode(pdf_file.read())
-	                res.values()[0]['attachments']=[(name,encoded_string)]
+					for pagenum in range(pdfreader.numPages):
+						pdfwriter.addPage(pdfreader.getPage(pagenum))
+
+					pdfwriter.encrypt(password)
+					resulltpdf = open('/tmp/'+str(value[0][0]),'wb')
+					pdfwriter.write(resulltpdf)
+					resulltpdf.close()
+					with open('/tmp/'+str(value[0][0]), "rb") as pdf_file:
+						encoded_string = base64.b64encode(pdf_file.read())
+					res.values()[0]['attachments']=[(name,encoded_string)]
 		return res
 
 
